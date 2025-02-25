c5aada6df6da96e83ea8618ef6df685ce1984159
= <=>
2025-02-25 12:37:23 +0100

    first_commit

---
+++ src/main/java/com/example/approche_imperative/ApprocheImperativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.approche_imperative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprocheImperativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprocheImperativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/approche_imperative/ApprocheImperativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.approche_imperative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprocheImperativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
