736ca0198928ccf8fb748b4aeee8ccf43c717fc0
nlauvray <noalauvray@gmail.com>
2025-02-25 15:46:54 +0100

    tp2

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -1,12 +1,15 @@
 package entites;
 
 public class Personne {
-    // Attributs
-    public String nom;
-    public String prenom;
-    public AdressePostale adresse;
+    private String nom;
+    private String prenom;
+    private AdressePostale adresse;
 
-    // Constructeur
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
     public Personne(String nom, String prenom, AdressePostale adresse) {
         this.nom = nom;
         this.prenom = prenom;
